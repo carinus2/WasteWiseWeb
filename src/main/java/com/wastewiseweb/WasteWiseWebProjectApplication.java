f0c9739dc3f868fe7a5776128bbce70ebe45603f
nistorcarina <carinanistor123321@gmail.com>
2024-03-10 13:53:10 +0200

    initialize the project

---
+++ src/main/java/com/wastewiseweb/WasteWiseWebProjectApplication.java
@@ -0,0 +1,14 @@
+package com.wastewiseweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteWiseWebProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(WasteWiseWebProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wastewiseweb/WasteWiseWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wastewiseweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteWiseWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
